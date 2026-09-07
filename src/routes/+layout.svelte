7fde228b4e940ca9455fb1049facbc98fa20b588
devmin8 <devmin8@outlook.com>
2026-09-07 17:38:38 -0400

    chore: add tailwindcss

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,13 @@
 <script lang="ts">
 	import favicon from '$lib/assets/favicon.svg';
 
+	import './layout.css';
+
 	let { children } = $props();
 </script>
 
 <svelte:head>
+	<title>Tracker</title>
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
