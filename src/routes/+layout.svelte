78fc60e3b94cc3b4de5a9c021ca1a41a3e5178f8
devmin8 <devmin8@outlook.com>
2026-09-07 17:53:49 -0400

    fix: font aliasing

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,7 @@
 	import { ModeWatcher } from 'mode-watcher';
 
 	import favicon from '$lib/assets/favicon.svg';
+	import geistLatin from '@fontsource-variable/geist/files/geist-latin-wght-normal.woff2?url';
 
 	import './layout.css';
 
@@ -11,6 +12,7 @@
 <svelte:head>
 	<title>Tracker</title>
 	<link rel="icon" href={favicon} />
+	<link rel="preload" href={geistLatin} as="font" type="font/woff2" crossorigin="anonymous" />
 </svelte:head>
 
 <ModeWatcher />
